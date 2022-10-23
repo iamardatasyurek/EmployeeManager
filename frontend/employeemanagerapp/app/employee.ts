66520a28db7259b653668e2387f61147dd6dbbf5
Arda Taşyürek <ardatasyurek@outlook.com>
2022-10-23 23:42:53 +0300

    Angular project is added

---
+++ frontend/employeemanagerapp/app/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee{
+    id:number;
+    firstName:string;
+    lastName:string;
+    jobTitle:string;
+    email:string;
+    phone:string;
+    imageUrl:string;
+    employeeCode:string;
+}
